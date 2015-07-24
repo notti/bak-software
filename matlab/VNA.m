2fefdd7e7b161695cfe84de527bb93d9c697c2db
Gernot Vormayr <gvormayr@gmail.com>
2015-07-24 12:33:41 +0200

    working filter; measurement script; sample data

--- matlab/VNA.m
+++ matlab/VNA.m
@@ -12,22 +12,39 @@ classdef VNA < handle
         function obj = VNA(address, port)
             obj.handle = visa('agilent', address);
             set(obj.handle,'InputBufferSize', 1e7);
-            set(obj.handle,'Timeout', 10);
+            set(obj.handle,'Timeout', 30);
             fopen(obj.handle);
             
             tracename = sprintf('S%d%d', port, port);
             
             fprintf(obj.handle, 'SOUR:POW -10');
-            fprintf(obj.handle, 'SENS:FOM:RANG:SWE:TYPE CW');
             fprintf(obj.handle, 'CALC:PAR:DEL:ALL');
             fprintf(obj.handle, 'CALC:PAR:DEF:EXT ''reflection'', ''%s''', tracename);
             fprintf(obj.handle, 'CALC:PAR:SEL ''reflection''');
             fprintf(obj.handle, 'DISP:WIND:TRAC:FEED ''reflection''');
+            fprintf(obj.handle, 'TRIG:SOUR MAN');
         end
         
         function delete(obj)
             fclose(obj.handle);
             delete(obj.handle);
+        end
+        
+        function continuous(obj, steps, time)
+            fprintf(obj.handle, 'SENS:SWE:POIN %d', steps);
+            fprintf(obj.handle, 'SENS:SWE:GEN ANAL');
+            fprintf(obj.handle, 'SENS:FOM:RANG:SWE:TYPE CW');
+            fprintf(obj.handle, 'SENS:SWE:TIME %g', time);
+        end
+        
+        function linear(obj, start, stop, steps, wait)
+            fprintf(obj.handle, 'SENS:SWE:TYPE LIN');
+            fprintf(obj.handle, 'SENS:FREQ:STAR %g', start);
+            fprintf(obj.handle, 'SENS:FREQ:STOP %g', stop);
+            fprintf(obj.handle, 'SENS:SWE:POIN %d', steps);
+            fprintf(obj.handle, 'SENS:SWE:GEN STEP');
+            fprintf(obj.handle, 'SENS:SWE:DWEL %g', wait);
+            fprintf(obj.handle, 'SENS:SWE:TIME:AUTO 1');
         end
         
         function set.freq(obj, value)
@@ -39,8 +56,10 @@ classdef VNA < handle
         end
         
         function value = get.y(obj)
-            value = mean(cell2mat(textscan(query(obj.handle, 'CALC:DATA? SDATA'), '%f %f', 'Delimiter', ',')));
-            value = value(1) + 1i*value(2);
+            fprintf(obj.handle, 'INIT:IMM; *OPC?');
+            fgets(obj.handle);
+            value = cell2mat(textscan(query(obj.handle, 'CALC:DATA? SDATA'), '%f %f', 'Delimiter', ','));
+            value = value(:,1) + 1i*value(:,2);
         end
     end
     
--- matlab/testfilter.m
+++ matlab/testfilter.m
@@ -11,9 +11,8 @@ sample = 100e6;
 
 f = designfilt('lowpassfir','FilterOrder',4096-1,'CutoffFrequency',25e6,'SampleRate',100e6,'Window','hamming');
 H = fft(impz(f)).';
+H_orig = H;
 ml507.H = int16(H.*32767./max([max(real(H)) max(imag(H))]));
-
-%%
 
 ml507.depth = 20000;
 ml507.transmitter.mul = 32767;
@@ -25,12 +24,15 @@ ml507.core.scale_schi(1:6) = [2 1 0 0 0 0];
 ml507.core.scale_cmul = 2;
 ml507.core.iq = 1;
 ml507.core.circular = 1;
+ml507.transmitter.resync();
 
 %%
 
 if ~ml507.running
     ml507.run();
 end
+
+vna.continuous(201, 100e-3);
 
 targets = [-1 1 1i];
 freqs = (-12:12)*1e6;
@@ -39,10 +41,12 @@ multipliers = zeros(length(freqs), length(targets));
 
 l=2;
 
+fprintf('\n');
+tic
 for i = 1:length(freqs)
     vna.freq = freqs(i) + center;
     for j = 1:length(targets)
-        [~, mul] = findTarget(ml507, targets(j), @()1/vna.y, 0.1, @status);
+        [~, mul] = findTarget(ml507, targets(j), @()1/mean(vna.y), 0.1, @status);
         multipliers(i,j) = mul;
         done = ((i-1)*length(targets)+j);
         for x=1:l
@@ -53,10 +57,10 @@ for i = 1:length(freqs)
 end
 
 fprintf('\n');
-
+toc
 %%
 
-which = 1;
+which = 2;
 
 fstep = sample/nfft;
 
@@ -94,3 +98,38 @@ cla;
 plot([abs(fftshift(H)).' abs(fftshift(H_orig)).'])
 
 ml507.H = int16(H.*32767./max([max(real(H)) max(imag(H))]));
+ml507.transmitter.mul = 32767;
+
+%%
+
+vna.linear(895e6, 905e6, 11, 0.5);
+
+vals = linspace(-1,1,11);
+[valsx, valsy] = meshgrid(vals,vals);
+vals = valsx+1i*valsy;
+
+results = zeros([size(vals) 11]);
+
+tic
+
+l = 1;
+fprintf('\n ');
+
+for i = 1:size(vals, 1)
+    for j = 1:size(vals, 2)
+        ml507.transmitter.mul = vals(i, j)*32767;
+        results(i, j, :) = vna.y;
+        for x=1:l
+            fprintf('\b');
+        end
+        done = ((i-1)*size(vals, 2)+j);
+        l=fprintf('[%*s] % 2.0f%%',-floor(numel(vals)/2),repmat('.',1,floor(done/2)),done/numel(vals)*100);
+    end
+end
+
+fprintf('\n');
+
+toc
+
+figure;
+smithchart(reshape(results(:,:,:),[size(results,1)*size(results,2) size(results,3)]).');
