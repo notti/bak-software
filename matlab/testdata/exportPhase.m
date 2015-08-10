64bcd7a24fc7741963defb398f7d19e1638582f3
Gernot Vormayr <gvormayr@gmail.com>
2015-08-10 23:57:16 +0200

    Updated phase data export routine

--- matlab/testdata/exportPhase.m
+++ matlab/testdata/exportPhase.m
@@ -5,6 +5,5 @@ result(:,2) = unwrap(angle(result(:,2)))*180/pi;
 dlmwrite('phase/single', result, '\t');
 clear;
 load('phasedriftAll.mat');
-res(:,1) = (res(:,1)-res(1,1))*24*3600;
-res(:,2:5) = unwrap(res(:,2:5))*180/pi;
-dlmwrite('phase/all', res, '\t');
+corrected(:,1) = (corrected(:,1) - corrected(1,1))*24*3600;
+dlmwrite('phase/all', [corrected(:,1) corrected(:,[2:4 7])*180/pi], '\t');
